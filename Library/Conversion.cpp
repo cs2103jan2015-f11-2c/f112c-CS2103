d4dd61459a769683d612433994b387d50c748cd8
kangter123 <kangter@gmail.com>
2015-03-06 15:07:59 +0800

    Updated Conversion.cpp

--- Library/Conversion.cpp
+++ Library/Conversion.cpp
@@ -9,3 +9,41 @@ Conversion::Conversion(void)
 Conversion::~Conversion(void)
 {
 }
+
+
+std::string Conversion::boolToString(bool){
+}
+
+bool Conversion::stringToBool (std::string){
+}
+
+
+
+std::string Conversion::eventToString(Event){
+}
+
+Event Conversion::stringToEvent(std::string){
+}
+
+
+
+int Conversion::monthToInt (std::string){
+}
+
+std::string Conversion::intToMonth (int){
+}
+
+
+
+std::string Conversion::intToDayOfWeek (int){
+}
+
+int dayOfWeekToInt (std::string){
+}
+
+
+
+std::string Conversion::intToString (int){
+}
+int Conversion::stringToInt (std::string){
+}
