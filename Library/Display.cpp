e1ae7f44df56601ef0332306cac3048460bd6d97
Wei Jee <owj_88@hotmail.com>
2015-03-08 14:37:36 +0800

    added display class for logic to UI

---
+++ Library/Display.cpp
@@ -0,0 +1,58 @@
+#include "Display.h"
+
+//constructor
+Display::Display() {
+	normalEvents.clear();
+	floatingEvents.clear();
+	feedbackEvents.clear();
+	mainDisplayStrings.clear();
+	floatingDisplayStrings.clear();
+	feedbackDisplayStrings.clear();
+}
+
+
+//getters
+vector<Event> Display::getNormalEvents() {
+	return normalEvents;
+}
+
+vector<Event> Display::getFloatingEvents() {
+	return floatingEvents;
+}
+
+vector<Event> Display::getFeedbackEvents() {
+	return feedbackEvents;
+}
+
+vector<string> Display::getMainDisplayStrings() {
+	return mainDisplayStrings;
+}
+
+vector<string> Display::getFloatingDisplayStrings() {
+	return floatingDisplayStrings;
+}
+
+vector<string> Display::getFeedbackDisplayStrings() {
+	return feedbackDisplayStrings;
+}
+
+
+//setters
+void Display::setNormalEvents(vector<Event> events) {
+	normalEvents = events;
+}
+
+void Display::setFloatingEvents(vector<Event> events) {
+	floatingEvents = events;
+	floatingEventsToString();
+}
+
+void Display::setFeedbackEvents(vector<Event> events) {
+	feedbackEvents = events;
+}
+
+void Display::floatingEventsToString() {
+	for (int i = 0; i < floatingEvents.size(); i++) {
+		floatingDisplayStrings.push_back(floatingEvents[i].getName());
+	}
+}
---
+++ Library/Display.h
@@ -0,0 +1,47 @@
+// Display keeps track of 3 x vector<Event> (1 each for floating, normal and feedback) and 3 x vector<string> (similar to event vectors)
+// When any of the vector<Event> is changed, it will update the corresponding vector<string>
+//		(e.g. if normalEvents is changed it will change mainDisplayStrings, floatingEvents<=>floatingDisplayStrings, feedbackEvents<=>feedbackDisplayStrings)
+// UI can get any of the vector<string> to display in the appropriate window, no need to get vector<Event>
+
+#pragma once
+
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "Event.h"
+
+
+class Display {
+private:
+	vector<Event> normalEvents;
+	vector<Event> floatingEvents;
+	vector<Event> feedbackEvents;
+	vector<string> mainDisplayStrings;
+	vector<string> floatingDisplayStrings;
+	vector<string> feedbackDisplayStrings;
+
+
+public:
+	//constructor, destructor
+	Display();
+	//~Display();
+	
+	//getters
+	vector<Event> getNormalEvents();
+	vector<Event> getFloatingEvents();
+	vector<Event> getFeedbackEvents();
+	vector<string> getMainDisplayStrings();
+	vector<string> getFloatingDisplayStrings();
+	vector<string> getFeedbackDisplayStrings();
+
+	//setters
+	void setNormalEvents(vector<Event> events);
+	void setFloatingEvents(vector<Event> events);
+	void setFeedbackEvents(vector<Event> events);
+
+	void normalEventsToString();
+	void floatingEventsToString();
+	void feedbackEventsToString();
+};
+
+#endif
