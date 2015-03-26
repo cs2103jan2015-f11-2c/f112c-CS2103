f68e88988b1febed7edf09248be329f50fb2cc2b
Wei Jee <owj_88@hotmail.com>
2015-03-26 22:47:37 +0800

    seriously

--- UnitTest/LogicTest.cpp
+++ UnitTest/LogicTest.cpp
@@ -13,8 +13,29 @@ namespace UnitTest
 		TEST_METHOD(Logic_isNumber_Test)
 		{
 			Logic logic;
+			//
 			Assert::AreEqual(logic.isNumber("3"), true);
+			//
 			Assert::AreEqual(logic.isNumber("3a"), false);
+		}
+
+	};
+
+	TEST_CLASS(CommandTest)
+	{
+	public:
+		
+		TEST_METHOD(ICommand_getIsFloating_Test)
+		{
+			/*Parser parser("add float;");
+			EventStorage eventStore;
+			Event addedEvent = parser.getEvent();
+			
+			ICommand* addCommand = new AddCommand(&eventStore, addedEvent);
+			Executor executor;
+			executor.execute(addCommand);
+
+			Assert::AreEqual(addCommand->getIsFloating(), true);*/
 		}
 
 	};
