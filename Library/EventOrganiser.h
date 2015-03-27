81cbd75d18f895dea585d40d7c298b081f5f2634
georgelam123 <a0113860@nus.edu.sg>
2015-03-27 21:44:13 +0800

    minor refract

--- Library/EventOrganiser.h
+++ Library/EventOrganiser.h
@@ -21,18 +21,8 @@ public:
 	
 	vector<Event> showAllNormalEvent(vector<Event> currentContent);
 	vector<Event> showAllFloatingEvent(vector<Event> currentFloatingContent);
-	vector<Event> showDay(int dayToShow, int monthToShow, int yearToShow, vector<Event> currentContent);
-	//vector<Event> showWeek(int dayToShow, int monthToShow, int yearToShow, vector<Event> eventsToFilter);
-	//vector<Event> showMonth(int monthToShow, int yearToShow, vector<Event> eventsToFilter);
-
 	vector<Event> sortEventVectorByDate(vector<Event> eventsToSort);
-	/*bool isLatterTimeSmaller(const Event eventTime1,const Event EventTime2);
-	bool isLatterYearSmaller(Event eventTime1, Event EventTime2);
-	bool isLatterMonthSmaller(Event eventTime1, Event EventTime2);
-	bool isLatterDaySmaller(Event eventTime1, Event EventTime2);
-	bool isLatterHourSmaller(Event eventTime1, Event EventTime2);
-	bool isLatterMinSmaller(Event eventTime1, Event EventTime2);
-*/
+
 	vector<struct tm> eventDateToVector(Event showEventDates);
 	vector<Event> showDateRange(Event eventWithStartEndTimes, vector<Event> eventsToFilter);
 	vector<Event> sortMarker(vector<Event> showResult);
