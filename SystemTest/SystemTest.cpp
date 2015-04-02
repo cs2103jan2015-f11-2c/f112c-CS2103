85e4170f0dcacc0e602a1874dc5efe0c9316fe8b
Joshua Che <joshua.chejy@gmail.com>
2015-04-02 18:18:06 +0800

    still working on systemtest

--- SystemTest/SystemTest.cpp
+++ SystemTest/SystemTest.cpp
@@ -8,6 +8,15 @@
 #include "Conversion.cpp"
 #include "EventFacade.cpp"
 #include "EventOrganiser.cpp"
+#include "EventModifier.cpp"
+#include "EventSearch.cpp"
+#include "EventStorage.cpp"
+#include "Parser.cpp"
+#include "ParserProcessor.cpp"
+#include "InputStringSplit.cpp"
+#include "ParserLog.cpp"
+#include "EventLog.cpp"
+#include "ParserExceptions.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -21,14 +30,23 @@ namespace SystemTest
 
 		TEST_METHOD(ADD_EVENT)
 		{
+			std::string textLine, testString;
+			std::string expectedString;
+
+			std::string expectedString;
+			std::vector<std::string> tempVector;
+
 			//Case 1: Add with no event name
 			Assert::AreEqual(l.executeUserInput("add "),false);
-			std::vector<std::string> tempVector = l.getFeedbackStrings();
-			std::string expectedString = "Error: No event name found. Please type ';' after an event name.";
+			tempVector = l.getFeedbackStrings();
+			expectedString = "Error: No event name found. Please type ';' after an event name.";
 			Assert::AreEqual(tempVector[0],expectedString);
 
 			//Case 1: Add floating
 			Assert::AreEqual(l.executeUserInput("add cs1;"),true);
+			expectedString = "Is Floating: 1\nName: cs1\nStart Year: 0\nStart Month: 0\nStart Day: 0\nStart Hour: 0\n";
+			expectedString += "Start Min: 0\nEnd Year: 0\nEnd Month: 0\nEnd Day: 0\nEnd Hour: 0\nEnd Min: 0\n";
+			expectedString += "Deadline: 0\nCompleted: 0\nImportance: 0\n";
 			
 			//Case 2: Add normal event
 			Assert::AreEqual(l.executeUserInput("add cs1; 23-24apr 3-4pm"),true);
