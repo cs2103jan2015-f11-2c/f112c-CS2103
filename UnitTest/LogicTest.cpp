f29bc735c221e04be0f279cf0a9182ae6e55ba04
Wei Jee <owj_88@hotmail.com>
2015-04-08 01:28:19 +0800

    logic unit tests

---
+++ UnitTest/LogicTest.cpp
@@ -0,0 +1,57 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Logic.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(LogicTest)
+	{
+	public:
+		Logic logic;
+
+		TEST_METHOD(Logic_isProperCommand_Test)
+		{
+			//test partition of valid parser command
+			Assert::AreEqual(logic.isProperCommand(Parser::ADD), true);
+		}
+
+
+		TEST_METHOD(Logic_isNumber_Test)
+		{
+			//test partition of string with only nondigits
+			string s = "ab!@";
+			Assert::AreEqual(logic.isNumber(s), false);
+
+
+			//test partition of string with only digits
+			s = "1234";
+			Assert::AreEqual(logic.isNumber(s), true);
+
+
+			//test partition of string with both digits and nondigits
+			s = "1234ab!@";
+			Assert::AreEqual(logic.isNumber(s), false);
+		}
+
+
+		TEST_METHOD(Logic_isSameDate_Test)
+		{
+			//test partition of both dates same
+			tm first, second;
+			first.tm_mday = 1;
+			first.tm_mon = 1;
+			first.tm_year = 115;
+			second.tm_mday = 1;
+			second.tm_mon = 1;
+			second.tm_year = 115;
+			Assert::AreEqual(logic.isSameDate(first, second), true);
+		}
+
+	};
+
+/**
+* @author A0111089L
+*/
+}
